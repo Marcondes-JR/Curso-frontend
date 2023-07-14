5fc7e79957fed87b5dd73ad4f4e30362815d0a2b
Marcondes Junior <sinesiojunior77@gmail.com>
2023-07-13 22:48:59 -0300

    ampli_tdjunto

---
+++ ampli/estudo_de_caso_ampli.js
@@ -0,0 +1,25 @@
+var hora = 9
+var minutos = 15
+var total_entrevistas = 0
+
+var saida = 17
+
+//horas
+for (i = hora; i < saida; i++) {
+
+  if ((i == 12) || (i == 13)) {
+    continue
+  }
+
+  //minutos
+  for (j = 0; j < 60; j = j + minutos) {
+    total_entrevistas++;
+    if (j == 0) {
+      console.log(`Entrevista #${total_entrevistas} => ${i}:${j}0`)
+    } else {
+      console.log(`Entrevista #${total_entrevistas} => ${i}:${j}`)
+
+    }
+  }
+
+}
